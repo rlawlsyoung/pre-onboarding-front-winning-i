71b9225ac93432a494f13a7c24b3669314af6a9b
Kim-Jin-Young <100749629+rlawlsyoung@users.noreply.github.com>
2022-10-15 19:28:21 +0900

    [Add] 헤더 및 사이드바 UI 구현 (#1)
    * [Add] 헤더 및 사이드바 구현
    
    * [Add] 사이드바 UI 구현

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,47 @@
-import styled from "styled-components";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import MenuIcon from './MenuIcon';
+import SideBar from './SideBar';
+import wi from '../../assets/wi.svg';
+import styled from 'styled-components';
 
 const Header = () => {
-  return <HeaderContainer></HeaderContainer>;
+  const [openSide, setOpenSide] = useState(false);
+
+  return (
+    <HeaderContainer>
+      <MenuIcon openSide={openSide} setOpenSide={setOpenSide} />
+      <Link>
+        <img src={wi} alt='위닝아이 로고' className='logo' />
+      </Link>
+      <SideBar openSide={openSide} />
+    </HeaderContainer>
+  );
 };
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  padding: 15px;
+  background-color: black;
+
+  .flex-center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .logo {
+    width: 225px;
+  }
+
+  @media screen and (max-width: 480px) {
+    .logo {
+      width: 175px;
+    }
+  }
+`;
 
 export default Header;
---
+++ src/components/header/SideBarMenu.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SideBarMenu = ({ icon, title, link }) => {
+  return (
+    <Link to={link}>
+      <SideBarMenuContainer className='flex-center hover'>
+        {icon} <h2>{title}</h2>
+      </SideBarMenuContainer>
+    </Link>
+  );
+};
+
+const SideBarMenuContainer = styled.li`
+  height: 80px;
+  margin: 10px 0;
+  color: black;
+  cursor: pointer;
+
+  h2 {
+    margin-left: 10px;
+    font-weight: 700;
+    font-size: 22px;
+  }
+`;
+
+export default SideBarMenu;
